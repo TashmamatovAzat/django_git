f432b0d2b7377e11875663c82616bec97e625d15
TashmamatovAzat <thisisnotspoon@gmail.com>
2023-04-17 11:57:43 +0600

    added User, Commit models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.username
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=255)
+    date_create = models.DateField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text[:30]
+
