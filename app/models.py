158d7256c6709d89bed656cc62737d11caee50b8
Aidai-Chinybaeva <chinybaeva1@gmail.com>
2023-04-17 12:10:06 +0600

    added Category, Item, models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
