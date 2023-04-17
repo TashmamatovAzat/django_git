748eb6ed96f6aa80cfd5263badeaa8651eaaedce
Aidai-Chinybaeva <chinybaeva1@gmail.com>
2023-04-17 12:16:19 +0600

    added new column to Categoty

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-
+    codename = models.CharField(max_length=50)
     def __str__(self):
         return self.name
 
